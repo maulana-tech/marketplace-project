a6bc54cd391979cb925b6a6d52d924b3dba85fb7
maulana-tech <firdausyah03@gmail.com>
2024-12-09 03:23:03 +0700

    mengubah blade php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -10,7 +11,43 @@ class FrontController extends Controller
     //
     public function index(){
         $products = Product::all();
-        return view ('front.index', [
+        $categories = Category::all();
+        return view('front.index', [
+            'products' => $products,
+            'categories' => $categories
+        ]);
+    }
+
+    public function details(Product $product){
+        $other_products = Product::where('id', '!=', $product->id)->get();
+        $creator_id = $product->creator_id;
+        $creator_products = Product::where('creator_id', $creator_id)->get();
+        return view('front.details', [
+            'product' => $product,
+            'other_products' => $other_products,
+            'creator_products' => $creator_products
+        ]);
+    }
+
+    public function category(Category $category){
+        $product_categories = Product::where('category_id', $category->id)->get();
+        return view('front.category', [
+            'category' => $category,
+            'product_categories' => $product_categories
+        ]);
+    }
+
+    public function search(Request $request){
+        $keyword = $request->input('keyword');
+
+        $products = Product::query()
+
+        ->where('name', 'LIKE', '%' . $keyword . '%')
+        ->orWhereHas('category', function ($query) use ($keyword){
+            $query->where('name', 'LIKE', '%' . $keyword . '%');
+        })->get();
+        
+        return view('front.search', [
             'products' => $products
         ]);
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProductController;
@@ -19,24 +20,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/details/{product:slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/category/{category}', [FrontController::class, 'category'])->name('front.category');
+Route::get('/search', [FrontController::class, 'search'])->name('front.search');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/checkout/{product:slug}', [CheckoutController::class, 'checkout'])->name('front.checkout');
+    Route::post('/checkout/store/{product:slug}', [CheckoutController::class, 'store'])->name('front.checkout.store');
+
+    Route::prefix('admin')->name('admin.')->group(function(){
         Route::resource('products', ProductController::class);
         Route::resource('product_orders', ProductOrderController::class);
 
-        Route::get('/transactions', [ProductOrderController::class, 'transactions' ])->name('product_orders.transactions');
-        Route::get('/transactions/details/{product_order}', [ProductOrderController::class, 'transactions_details' ])->name
-        ('product_orders.transactions.details');
+        Route::get('/transactions', [ProductOrderController::class, 'transactions'])->name('product_orders.transactions');
+        Route::get('/transactions/details/{productOrder}', [ProductOrderController::class, 'transactions_details'])->name('product_orders.transactions.details');
 
-        Route::get('/download/file/{productOrder}', [ProductOrderController::class, 'download_file'])
-        ->name('product_orders.download')->middleware('throttle:1,1');
+        Route::get('/download/file/{productOrder}', [ProductOrderController::class, 'download_file'])->name('product_orders.download')->middleware('throttle:1,1');
 
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     });
 });
 
