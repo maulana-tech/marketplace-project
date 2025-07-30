7c52aa9f6eafd52ad015506ebc031cf82f1b3710
maulana-tech <firdaussyah03@gmail.com>
2025-07-30 20:48:02 +0700

    update ProductOrderController

--- app/Http/Controllers/ProductOrderController.php
+++ app/Http/Controllers/ProductOrderController.php
@@ -38,31 +38,31 @@ class ProductOrderController extends Controller
         ]);
     }
 
-    public function download_file(ProductOrder $productOrder)
-    {
-        $user_id = Auth::id();
-        $product_id = $productOrder->product_id;
+    //public function download_file(ProductOrder $productOrder)
+    //{
+        //$user_id = Auth::id();
+        //$product_id = $productOrder->product_id;
 
-        $paidTransactionExist = ProductOrder::where('buyer_id', $user_id)
-        ->where('product_id', $product_id)
-        ->where('is_paid', 1)
-        ->first();
+        //$paidTransactionExist = ProductOrder::where('buyer_id', $user_id)
+        //->where('product_id', $product_id)
+        //->where('is_paid', 1)
+        //->first();
 
-        if(!$paidTransactionExist) {
-            session()->flash('error', 'You must pay for this download first');
-            return redirect()->back();
-        }
+        //if(!$paidTransactionExist) {
+        //    session()->flash('error', 'You must pay for this download first');
+        //    return redirect()->back();
+        //}
 
-        $productDetails = Product::find($product_id);
+        //$productDetails = Product::find($product_id);
 
-        $filePath = $productDetails->path_file;
+        //$filePath = $productDetails->path_file;
 
-        if(!Storage::disk('public')->exists($filePath)) {
-            abort(404);
-        }
+        //if(!Storage::disk('public')->exists($filePath)) {
+        //    abort(404);
+        //}
 
-        return Storage::disk('public')->download($filePath);
-    }
+        //return Storage::disk('public')->download($filePath);
+    //}
 
     /**
      * Show the form for creating a new resource.
@@ -105,8 +105,11 @@ class ProductOrderController extends Controller
     public function update(Request $request, ProductOrder $productOrder)
     {
         //
-        $productOrder->update(['is_paid' => true]);
-        return redirect()->back()->with('message', 'Order has been marked as paid');
+        $productOrder->update([
+            'is_paid' => true,
+            'status' => 'success'
+        ]);
+        return redirect()->back()->with('message', 'Order has been approved and marked as success');
     }
 
     /**
